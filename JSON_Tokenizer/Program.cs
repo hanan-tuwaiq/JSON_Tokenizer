7f59c5b3ba718fd0b88aca27027f24bf41f0114e
hanan-tuwaiq <hanan.hakami@tuwaiq.edu.sa>
2021-04-22 23:05:41 +0300

    initial commit

---
+++ JSON_Tokenizer/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JSON_Tokenizer
+{
+    public delegate bool InputCondition(Input input);
+    public class Input
+    {
+        private readonly string input;
+        private readonly int length;
+        private int position;
+        private int lineNumber;
+        //Properties
+        public int Length
+        {
+            get
+            {
+                return this.length;
+            }
+        }
+        public int Position
+        {
+            get
+            {
+                return this.position;
+            }
+        }
+        public int NextPosition
+        {
+            get
+            {
+                return this.position + 1;
+            }
+        }
+        public int LineNumber
+        {
+            get
+            {
+                return this.lineNumber;
+            }
+        }
+        public char Character
+        {
+            get
+            {
+                if (this.position > -1) return this.input[this.position];
+                else return '\0';
+            }
+        }
+        public Input(string input)
+        {
+            this.input = input;
+            this.length = input.Length;
+            this.position = -1;
+            this.lineNumber = 1;
+        }
+        public bool hasMore(int numOfSteps = 1)
+        {
+            if (numOfSteps <= 0) throw new Exception("Invalid number of steps");
+            return (this.position + numOfSteps) < this.length;
+        }
+        public bool hasLess(int numOfSteps = 1)
+        {
+            if (numOfSteps <= 0) throw new Exception("Invalid number of steps");
+            return (this.position - numOfSteps) > -1;
+        }
+        //callback -> delegate
+        public Input step(int numOfSteps = 1)
+        {
+            if (this.hasMore(numOfSteps))
+                this.position += numOfSteps;
+            else
+            {
+                throw new Exception("There is no more step");
+            }
+            return this;
+        }
+        public Input back(int numOfSteps = 1)
+        {
+            if (this.hasLess(numOfSteps))
+                this.position -= numOfSteps;
+            else
+            {
+                throw new Exception("There is no more step");
+            }
+            return this;
+        }
+        public Input reset() { return this; }
+        public char peek(int numOfSteps = 1)
+        {
+            if (this.hasMore()) return this.input[this.NextPosition];
+            return '\0';
+        }
+        public string loop(InputCondition condition)
+        {
+            string buffer = "";
+            while (this.hasMore() && condition(this))
+                buffer += this.step().Character;
+            return buffer;
+        }
+    }
+    public class Token
+    {
+        public int Position { set; get; }
+        public int LineNumber { set; get; }
+        public string Type { set; get; }
+        public string Value { set; get; }
+        public Token(int position, int lineNumber, string type, string value)
+        {
+            this.Position = position;
+            this.LineNumber = lineNumber;
+            this.Type = type;
+            this.Value = value;
+        }
+    }
+    public abstract class Tokenizable
+    {
+        public abstract bool tokenizable(Tokenizer tokenizer);
+        public abstract Token tokenize(Tokenizer tokenizer);
+    }
+    public class Tokenizer
+    {
+        public List<Token> tokens;
+        public bool enableHistory;
+        public Input input;
+        public Tokenizable[] handlers;
+        public Tokenizer(string source, Tokenizable[] handlers)
+        {
+            this.input = new Input(source);
+            this.handlers = handlers;
+        }
+        public Tokenizer(Input source, Tokenizable[] handlers)
+        {
+            this.input = source;
+            this.handlers = handlers;
+        }
+        public Token tokenize()
+        {
+            foreach (var handler in this.handlers)
+                if (handler.tokenizable(this)) return handler.tokenize(this);
+            return null;
+        }
+        public List<Token> all() { return null; }
+    }
+    public class IdTokenizer : Tokenizable
+    {
+        private List<string> keywords;
+        public IdTokenizer(List<string> keywords)
+        {
+            this.keywords = keywords;
+        }
+        public override bool tokenizable(Tokenizer t)
+        {
+            char currentCharacter = t.input.peek();
+            //Console.WriteLine(currentCharacter);
+            return Char.IsLetter(currentCharacter) || currentCharacter == '_';
+        }
+        static bool isId(Input input)
+        {
+            char currentCharacter = input.peek();
+            return Char.IsLetterOrDigit(currentCharacter) || currentCharacter == '_';
+        }
+        public override Token tokenize(Tokenizer t)
+        {
+            return new Token(t.input.Position, t.input.LineNumber,
+                "identifier", t.input.loop(isId));
+        }
+    }
+    public class NumberTokenizer : Tokenizable
+    {
+        public override bool tokenizable(Tokenizer t)
+        {
+            return Char.IsDigit(t.input.peek());
+        }
+        static bool isDigit(Input input)
+        {
+            return Char.IsDigit(input.peek());
+        }
+        public override Token tokenize(Tokenizer t)
+        {
+            return new Token(t.input.Position, t.input.LineNumber,
+                "number", t.input.loop(isDigit));
+        }
+    }
+    public class WhiteSpaceTokenizer : Tokenizable
+    {
+        public override bool tokenizable(Tokenizer t)
+        {
+            return Char.IsWhiteSpace(t.input.peek());
+        }
+        static bool isWhiteSpace(Input input)
+        {
+            return Char.IsWhiteSpace(input.peek());
+        }
+        public override Token tokenize(Tokenizer t)
+        {
+            return new Token(t.input.Position, t.input.LineNumber,
+                "whitespace", t.input.loop(isWhiteSpace));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tokenizer t = new Tokenizer(new Input("hanan jj"), new Tokenizable[] {
+                new WhiteSpaceTokenizer(),
+                new IdTokenizer(new List<string>
+                {
+                    "if","else","for","fun","return"
+                }),
+                new NumberTokenizer()
+            }); ;
+            Token token = t.tokenize();
+            while (token != null)
+            {
+                Console.WriteLine(token.Value);
+                token = t.tokenize();
+            }
+        }
+    }
+}
